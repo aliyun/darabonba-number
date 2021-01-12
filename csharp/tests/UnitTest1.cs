02d02d15eb149107f2ea69c7f1fd6945d5f0233b
liaoyustudent <wb-ly617236@alibaba-inc.com>
2021-01-12 18:16:40 +0800

    supported csharp

---
+++ csharp/tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using AlibabaCloud.DarabonbaNumber;
+using Xunit;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_ParseInt()
+        {
+            Assert.Equal(2, NumberUtil.ParseInt("2"));
+        }
+
+        [Fact]
+        public void Test_ParseFloat()
+        {
+            Assert.Equal(2f, NumberUtil.ParseFloat("2"));
+        }
+
+        [Fact]
+        public void Test_ParseDouble()
+        {
+            Assert.Equal(2d, NumberUtil.ParseDouble("2"));
+        }
+    }
+}
