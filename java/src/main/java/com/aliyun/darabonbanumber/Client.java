11327add32837dc31698435f63d74d380c7c0e28
yndu13 <yndu13@gmail.com>
2021-01-14 12:34:07 +0800

    add java

---
+++ java/src/main/java/com/aliyun/darabonbanumber/Client.java
@@ -0,0 +1,19 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.darabonbanumber;
+
+import com.aliyun.tea.*;
+
+public class Client {
+
+    public static Integer parseInt(String raw) throws Exception {
+        return Integer.parseInt(raw);
+    }
+
+    public static Float parseFloat(String raw) throws Exception {
+        return Float.parseFloat(raw);
+    }
+
+    public static Double parseDouble(String raw) throws Exception {
+        return Double.parseDouble(raw);
+    }
+}
