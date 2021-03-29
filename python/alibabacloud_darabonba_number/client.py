cb7c361c4a53a8db82accd6fd43f6554ad1970d2
824750130 <e824750130@qq.com>
2021-03-29 10:03:44 +0800

    Add logic operation method

--- python/alibabacloud_darabonba_number/client.py
+++ python/alibabacloud_darabonba_number/client.py
@@ -6,26 +6,95 @@ class Client:
     """
     This is a number module
     """
+
     @staticmethod
     def parse_int(
-        raw: str,
+            raw: str,
     ) -> int:
         return int(raw)
 
     @staticmethod
     def parse_long(
-        raw: str,
+            raw: str,
     ) -> int:
         return int(raw)
 
     @staticmethod
     def parse_float(
-        raw: str,
+            raw: str,
     ) -> float:
         return float(raw)
 
     @staticmethod
     def parse_double(
-        raw: str,
+            raw: str,
     ) -> float:
         return float(raw)
+
+    @staticmethod
+    def itol(
+        val: int,
+    ) -> int:
+        return val
+
+    @staticmethod
+    def ltoi(
+        val: int,
+    ) -> int:
+        return val
+
+    @staticmethod
+    def add(
+        val1: int,
+        val2: int,
+    ) -> int:
+        return val1 + val2
+
+    @staticmethod
+    def sub(
+        val1: int,
+        val2: int,
+    ) -> int:
+        return val1 - val2
+
+    @staticmethod
+    def mul(
+        val1: int,
+        val2: int,
+    ) -> int:
+        return val1 * val2
+
+    @staticmethod
+    def div(
+        val1: int,
+        val2: int,
+    ) -> float:
+        return val1 / val2
+
+    @staticmethod
+    def gt(
+        val1: int,
+        val2: int,
+    ) -> bool:
+        return val1 > val2
+
+    @staticmethod
+    def gte(
+        val1: int,
+        val2: int,
+    ) -> bool:
+        return val1 >= val2
+
+    @staticmethod
+    def lt(
+            val1: int,
+            val2: int,
+    ) -> bool:
+        return val1 < val2
+
+    @staticmethod
+    def lte(
+            val1: int,
+            val2: int,
+    ) -> bool:
+        return val1 <= val2
--- python/test/test_client.py
+++ python/test/test_client.py
@@ -17,3 +17,41 @@ class TestClient(unittest.TestCase):
     def test_parse_float(self):
         self.assertEqual(100.0, Client.parse_float('100'))
         self.assertEqual(100.1, Client.parse_float('100.1'))
+
+    def test_itol(self):
+        self.assertEqual(100, Client.itol(100))
+
+    def test_ltoi(self):
+        self.assertEqual(100, Client.ltoi(100))
+
+    def test_add(self):
+        self.assertEqual(200, Client.add(100, 100))
+
+    def test_sub(self):
+        self.assertEqual(50, Client.sub(100, 50))
+
+    def test_mul(self):
+        self.assertEqual(100, Client.mul(10, 10))
+
+    def test_div(self):
+        self.assertEqual(10.0, Client.div(100, 10))
+
+    def test_gt(self):
+        self.assertTrue(Client.gt(2, 1))
+        self.assertFalse(Client.gt(1, 2))
+        self.assertFalse(Client.gt(1, 1))
+
+    def test_gte(self):
+        self.assertTrue(Client.gte(2, 1))
+        self.assertFalse(Client.gte(1, 2))
+        self.assertTrue(Client.gte(1, 1))
+
+    def test_lt(self):
+        self.assertTrue(Client.lt(1, 2))
+        self.assertFalse(Client.lt(2, 1))
+        self.assertFalse(Client.lt(1, 1))
+
+    def test_lte(self):
+        self.assertTrue(Client.lte(1, 2))
+        self.assertFalse(Client.lte(2, 1))
+        self.assertTrue(Client.lte(1, 1))
