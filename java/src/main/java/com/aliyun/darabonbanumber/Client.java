218df267a40e53ae85f4fb4fefbe71827a357eeb
nanhe <papaya.dyn@alibaba-inc.com>
2022-01-26 20:27:24 +0800

    add java func

--- java/src/main/java/com/aliyun/darabonbanumber/Client.java
+++ java/src/main/java/com/aliyun/darabonbanumber/Client.java
@@ -1,4 +1,5 @@
 // This file is auto-generated, don't edit it. Thanks.
+// This file is auto-generated, don't edit it. Thanks.
 package com.aliyun.darabonbanumber;
 
 import com.aliyun.tea.*;
@@ -19,5 +20,45 @@ public class Client {
 
     public static Double parseDouble(String raw) throws Exception {
         return Double.parseDouble(raw);
+    }
+
+    public static Long itol(Integer val) throws Exception {
+        return val.longValue();
+    }
+
+    public static Integer ltoi(Long val) throws Exception {
+        return val.intValue();
+    }
+
+    public static Long add(Long val1, Long val2) throws Exception {
+        return val1 + val2;
+    }
+
+    public static Long sub(Long val1, Long val2) throws Exception {
+        return val1 - val2;
+    }
+
+    public static Long mul(Long val1, Long val2) throws Exception {
+        return val1 * val2;
+    }
+
+    public static Double div(Long val1, Long val2) throws Exception {
+        return (double)(val1 / val2);
+    }
+
+    public static Boolean gt(Long val1, Long val2) throws Exception {
+        return val1 > val2;
+    }
+
+    public static Boolean gte(Long val1, Long val2) throws Exception {
+        return val1 >= val2;
+    }
+
+    public static Boolean lt(Long val1, Long val2) throws Exception {
+        return val1 < val2;
+    }
+
+    public static Boolean lte(Long val1, Long val2) throws Exception {
+        return val1 <= val2;
     }
 }
